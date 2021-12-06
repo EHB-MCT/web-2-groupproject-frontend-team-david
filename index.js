cfde82f6dcab0d2e8aaef78e353d4a98edddfa93
kevser-tr <kevser.tanrikulu@student.ehb.be>
2021-12-06 22:33:36 +0100

    styling

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 async function saveChallenge(e) {
-    e.preventDefault()
-    let myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+  e.preventDefault()
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-    let data = {
-      name: document.getElementById("challenge_name").value,
-      "points": "film",
-      "course": "WEB II",
-      "session": "123456  een bordspel"
-    };
+  let data = {
+    name: document.getElementById("challenge_name").value,
+    "points": "film",
+    "course": "WEB II",
+    "session": "123456  een bordspel"
+  };
 
-    let requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: data,
-      redirect: 'follow'
-    };
 
-    console.log(data.name);
-    // fetch("https://web2-backend-teamdavid.herokuapp.com/api/saveBoardgame", requestOptions)
-    //   .then(response => response.text())
-    //   .then(result => console.log(result))
-    //   .catch(error => console.log('error', error));
-
+  console.log(data.name);
 }
 
 document.getElementById("challenge").addEventListener("submit", e => {
@@ -50,7 +39,7 @@ document.getElementById("challenge").addEventListener("submit", e => {
     .catch(error => console.log('error', error));
 })
 
-async function getData()  {
+async function getData() {
 
   let resp = await fetch("https://web2-backend-teamdavid.herokuapp.com/api/challenges");
 
@@ -58,41 +47,27 @@ async function getData()  {
 
 }
 
-async function showData () {
+async function showData() {
 
-      let data = await getData();
-  
-      data.DEV.forEach(DEV => {
-        console.log(DEV)
-        let html = `
-        <h1>Name</h1>
-        <p>${DEV.name}</p>
-        <h1>points</h1>
-        <p>${DEV.points}</p>
-        <h1>course</h1>
-        <p>${DEV.course}</p>
-        <h1>session</h1>
-        <p>${DEV.session}</p>
-        `  
-        document.getElementById("list").insertAdjacentHTML('beforeend', html) 
-      })
-  
-      data.WEB.forEach(WEB => {
-        console.log(WEB)
-        let html = `
-        <h1>Name</h1>
-        <p>${WEB.name}</p>
-        <h1>points</h1>
-        <p>${WEB.points}</p>
-        <h1>course</h1>
-        <p>${WEB.course}</p>
-        <h1>session</h1>
-        <p>${WEB.session}</p>
-        `  
-        document.getElementById("list").insertAdjacentHTML('beforeend', html)
-  
-      })
-  
-    }
-  
-    showData()
+  let data = await getData();
+  console.log(data);
+  data.forEach(WEB => {
+    console.log(WEB)
+    let html = `<div id="item"><img id="edit" src="./img/iconmonstr-pencil-14-240.png" alt=""><img id="delete" src="./img/iconmonstr-trash-can-1-240.png" alt=""><h1>Name</h1><p>${WEB.name}</p><h1>points</h1><p>${WEB.points}</p><h1>course</h1><p>${WEB.course}</p><h1>session</h1><p>${WEB.session}</p></div>`
+    document.getElementById("list").insertAdjacentHTML('beforeend', html)
+
+  })
+
+}
+async function deleteData(e) {
+  document.getElementById("delete").addEventListener("click", e)
+  let requestOptions = {
+    method: 'DELETE',
+    headers: myHeaders,
+    body: data,
+    redirect: 'follow'
+
+  };
+  fetch("https://web2-backend-teamdavid.herokuapp.com/api/challenges/61a5faa5b28bc82729248efa", requestOptios)
+}
+showData()
